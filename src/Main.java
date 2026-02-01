1fc8f2ffe4ceb9d989db8ff3fa4af074d49db1c1
S.M. Kafi <smkafi4@gmail.com>
2026-02-01 10:35:56 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+
+    public static int binary(int[] arr, int key) {
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            }
+            else if (arr[mid] < key) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return -1; // not found
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+        int key = 30;
+
+        int result = binary(arr, key);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
